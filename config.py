a867024d1fda86400bc7faf4ed5b03c103671497
Marshal <marshal@xaker.ru>
2018-09-09 16:19:20 +0300

    Fixed DB port

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ telegram_token = os.environ.get('TELEGRAM_BOT_TOKEN')
 
 DB = {
     'host': os.environ.get('DB_HOST'),
-    'port': os.environ.get('DB_PORT'),
+    'port': int(os.environ.get('DB_PORT')),
     'user': os.environ.get('DB_USER'),
     'password': os.environ.get('DB_PASSWORD'),
     'db': os.environ.get('DB_NAME'),
